7502a198c7944de20573a6fa4fcbb9c13cdf339f
Ivanov Evhenii <ivanov191192@gmail.com>
2021-01-08 23:02:07 +0200

    fix

--- src/timer.js
+++ src/timer.js
@@ -4,33 +4,62 @@ const refs = {
   minsRef: document.querySelector('span[data-value="mins"]'),
   secsRef: document.querySelector('span[data-value="secs"]'),
 };
-function timer() {
-  const targetDate = Date.parse(new Date('Jan 1, 2022'));
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+    this.days;
+    this.hours;
+    this.mins;
+    this.secs;
+  }
+  getIdDate() {
+    this.targetDate = Date.parse(new Date(this.targetDate));
+    this.getTimer();
+  }
+  getTimer() {
+    setInterval(() => {
+      const currentDate = Date.now();
+      const deltaTime = this.targetDate - currentDate;
+      this.timing(deltaTime);
+    }, 1000);
+  }
 
-  setInterval(function () {
-    const date = Date.now();
-    const delay = targetDate - date;
-    timing(delay);
-  }, 1000);
+  updateTimer() {
+    setInterval(() => {
+      if (this.days <= 0 && this.hours <= 0 && this.mins < 0) {
+        this.targetDate = new Date(
+          Date.parse(new Date()) + 365 * 24 * 60 * 60 * 1000,
+        );
+        const currentDate = Date.now();
+        const deltaTime = this.targetDate - currentDate;
+        this.timing(deltaTime);
+      }
+    }, 1000);
+  }
+  timing(time) {
+    this.days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    refs.daysRef.textContent = `${this.days}`;
+
+    this.hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    );
+    refs.hoursRef.textContent = `${this.hours}`;
+
+    this.mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    refs.minsRef.textContent = `${this.mins}`;
+    this.secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+    refs.secsRef.textContent = `${this.secs}`;
+    this.updateTimer();
+  }
+
+  pad(time) {
+    return String(time).padStart(2, '0');
+  }
 }
 
-function timing(time) {
-  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-  refs.daysRef.textContent = `${days}`;
-
-  const hours = pad(
-    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-  );
-  refs.hoursRef.textContent = `${hours}`;
-
-  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-  refs.minsRef.textContent = `${mins}`;
-
-  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-  refs.secsRef.textContent = `${secs}`;
-}
-
-function pad(value) {
-  return String(value).padStart(2, '0');
-}
-timer();
+const newYearTimer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Jan 1, 2022 '),
+});
+newYearTimer.getIdDate();
